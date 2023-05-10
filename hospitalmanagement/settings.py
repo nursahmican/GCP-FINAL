8452c4b416f63636a81247da7455e41cddbde1d2
nursahmican <77007760+nursahmican@users.noreply.github.com>
2023-05-10 22:30:47 +0300

    firstinit

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -54,8 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-        'hospital',
-        'widget_tweaks',
+    'hospital',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
